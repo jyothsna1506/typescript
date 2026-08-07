d4bf07c0eb47a0a13d21217752b01728e4add607
jyothsna1506 <jyothsnanavudu@gmail.com>
2026-08-07 08:31:20 +0530

    Update Exp2_1.ts

--- src/Exp2_1.ts
+++ src/Exp2_1.ts
@@ -1,16 +1,9 @@
-// Program to demonstrate Functions in TypeScript
-
-//Function with Parameters and Explicit Return Type
 function add(a: number, b: number): number {
     return a + b;
 }
-
-//Function with Default Parameter
 function greet(name: string = "Guest"): void {
     console.log("Welcome " + name);
 }
-
-//Function with Optional Parameter
 function displayStudent(name: string, age?: number): void {
     if (age !== undefined) {
         console.log("Name: " + name + ", Age: " + age);
@@ -18,8 +11,6 @@ function displayStudent(name: string, age?: number): void {
         console.log("Name: " + name);
     }
 }
-
-//Function with Rest Parameters
 function totalMarks(...marks: number[]): number {
     let sum = 0;
     for (let mark of marks) {
@@ -27,14 +18,9 @@ function totalMarks(...marks: number[]): number {
     }
     return sum;
 }
-
-//Function Calls
 console.log("Addition =", add(10, 20));
-
 greet("Jyothsna");
 greet();
-
 displayStudent("Jyothsna", 20);
 displayStudent("Sai");
-
 console.log("Total Marks =", totalMarks(85, 90, 95));
