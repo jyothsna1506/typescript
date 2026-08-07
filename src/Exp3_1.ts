1af9069c5f3fee05ee71e8031434d9cf60c5ea34
jyothsna1506 <jyothsnanavudu@gmail.com>
2026-08-07 09:10:52 +0530

    Update Exp3_1.ts

--- src/Exp3_1.ts
+++ src/Exp3_1.ts
@@ -1,26 +1,16 @@
-// Program to demonstrate Class Implementation with Constructors
-
 class FixedDeposit {
-
-    // ---------------- Properties ----------------
     accountHolder: string;
     amount: number;
     years: number;
     interestRate: number;
-
-    // ---------------- Constructor Overloading Signatures ----------------
     constructor();
     constructor(accountHolder: string, amount: number, years: number, interestRate: number);
-
-    // ---------------- Constructor Logic ----------------
     constructor(
         accountHolder?: string,
         amount?: number,
         years?: number,
         interestRate?: number
     ) {
-
-        // Default Constructor Logic
         if (
             accountHolder === undefined &&
             amount === undefined &&
@@ -32,8 +22,6 @@ class FixedDeposit {
             this.years = 1;
             this.interestRate = 6.5;
         }
-
-        // Overloaded Constructor Logic
         else {
             this.accountHolder = accountHolder!;
             this.amount = amount!;
@@ -41,13 +29,9 @@ class FixedDeposit {
             this.interestRate = interestRate!;
         }
     }
-
-    // ---------------- Method ----------------
     calculateMaturity(): number {
         return this.amount + (this.amount * this.interestRate * this.years) / 100;
     }
-
-    // ---------------- Display Account Summary ----------------
     displaySummary(): void {
         console.log("Account Holder :", this.accountHolder);
         console.log("Deposit Amount :", this.amount);
@@ -57,22 +41,13 @@ class FixedDeposit {
         console.log("-------------------------------");
     }
 }
-
-// ---------------- Using Default Constructor ----------------
 let fd1 = new FixedDeposit();
-
 console.log("Using Default Constructor");
 fd1.displaySummary();
-
-// ---------------- Using Overloaded Constructor ----------------
 let fd2 = new FixedDeposit("Jyothsna", 50000, 3, 7.5);
-
 console.log("Using Overloaded Constructor");
 fd2.displaySummary();
-
-// ---------------- Updating Properties after Construction ----------------
 fd1.accountHolder = "Sai";
 fd1.amount = 30000;
-
 console.log("After Updating Properties");
 fd1.displaySummary();
